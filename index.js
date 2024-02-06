6a57ce699065c42db06ad3190a06f60add0509c3
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-02-06 11:29:19 +0530

    added Toast

--- index.js
+++ index.js
@@ -1,39 +1,103 @@
-function validate(){
-    var user=document.getElementById('username').value;
-    var email=document.getElementById('email').value;
-    var password=document.getElementById('password').value;
-    var conformPassword=document.getElementById('confirm-password').value;
-    checkUsername(user) 
-    checkEmail(email) 
-    checkPassword(password) 
-    checkVal(password,conformPassword) 
+function validate() {
+  var user = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var conformPassword = document.getElementById("confirm-password").value;
+  checkUsername(user);
+  checkEmail(email);
+  checkPassword(password);
+  checkVal(password, conformPassword);
 }
-  function checkUsername(user){
-        if(user.length > 7){
-            document.getElementById('username-error').innerText = ""
-        }else{
-            document.getElementById('username-error').innerText = "username must be 8 character"
-        }
-    }
+function error() {}
+function checkUsername(user) {
+  const valid =
+    user.length > 7
+      ? ""
+      : Toastify({
+          text: "Username must be at least 8 characters",
+          className: "error",
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "left",
+          stopOnFocus: true,
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+        }).showToast();
+}
 
-  function checkEmail(email) {
-        if(email.length > 8 && email.includes('@gmail.com')){
-            document.getElementById('email-error').innerText = ""
-        }else{
-            document.getElementById('email-error').innerText = "Enter a valid email eg:username@gmail.com"
-        }
-    }
-    function checkPassword(password){
-        if(password.length > 8 && password.includes('.')){
-            document.getElementById('password-error').innerText = ""
-        }else{
-            document.getElementById('password-error').innerText = "password must be 8 letters long and also includes ."
-        } 
-    }
-    function checkVal(password,conformPassword){
-        if(password=== password && conformPassword){
-            document.getElementById('conformPassword-error').innerText = ""
-        }else{
-            document.getElementById('conformPassword-error').innerText = "Password do not match!"
-        }
-    }
+function checkEmail(email) {
+  const emailVal =
+    email.length > 8 && email.includes("@gmail.com")
+      ? ""
+      : Toastify({
+          text: "Enter a valid email eg:username@gmail.com",
+          className: "error",
+          duration: 3000,
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "left", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+        }).showToast();
+}
+function checkPassword(password) {
+  const passwordVal =
+    password.length > 8 && password.includes(".")
+      ? " "
+      : Toastify({
+          text: "password must be 8 letters long and also includes .",
+          className: "error",
+          duration: 3000,
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "left", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+        }).showToast();
+}
+function checkVal(password, conformPassword) {
+  const conformPasswordVal =
+    password === password && conformPassword.length
+      ? ""
+      : Toastify({
+          text: "password did not match",
+          className: "error",
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "left", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+        }).showToast();
+}
+
+const hell = document.getElementById("hell");
+hell.addEventListener("click", () => {
+  Toastify({
+    text: "This is a toast",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
+});
